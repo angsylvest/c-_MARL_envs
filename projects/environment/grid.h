4ea3219da709771860856cd84c7f84d055315f04
Angel Sylvester <sylve057@umn.edu>
2024-05-22 11:16:23 -0500

    prog updates to marl env c++

--- projects/environment/grid.h
+++ projects/environment/grid.h
@@ -5,17 +5,38 @@
 
 class GridEnvironment {
 public:
-    int num_rows;
-    int num_cols;
+    list<Particle> agents; 
+    // communication channel dim 
+    int dim_c; 
+    // pos dimensionality 
+    int dim_pos; 
+    // color dimensionality
+    int color_dim; 
+    // sim timestep 
+    float dt; 
+    // physical dampling
+    int damping; 
+    // contact force params 
+    float contact_force;
+    float contact_margin;  
+    // other gen params
+    int num_rows; 
+    int num_cols; 
     int** arrayxy;
 
     // constructor 
     GridEnvironment(int rows, int cols);
-
     // destructor
     ~GridEnvironment();
-
     void printGrid();
+    list<Particle> returnEntities(); 
+    void step(); 
+    float calcActionForce(); 
+    float calcEnvironmentForce(); 
+    vector<float> getCollisionForce(Particle a, Particle b); 
+    void updateAgentState(list<Particle> agents); 
+
+
 };
 
 #endif
